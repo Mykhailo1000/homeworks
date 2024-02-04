22dc8b338c1dfaf7781783efd742ddcf5a2d2c79
Mykhailo <mykhailokhomiak@gmail.com>
2024-02-04 14:05:32 +0200

    feat: 1 homework

---
+++ js/script.js
@@ -0,0 +1,50 @@
+let age = 25;
+console.log("13" + age);
+
+
+
+let name = "Mykhailo";
+console.log("Mykhailo" + name);
+
+
+
+let isStudent = true;
+console.log("Чи я студент? " + isStudent);
+
+
+
+let myString = "Життя прекрасне!";
+console.log("Моя улюблена цитата: " + myString);
+
+
+
+let myNumber = 42;
+myNumber += 10;
+console.log("42" + myNumber);
+
+
+
+let myNull = null;
+console.log("null" + myNull);
+
+
+
+let userName = prompt("Будь ласка введіть своє ім'я:");
+alert("Привіт, " + userName + "!");
+
+
+if (confirm("Ви впевнені, що хочете продовжити?")) {
+  alert("Дякую за підтвердження!");
+} else {
+  alert("Дію відмінено!");
+}
+
+
+
+
+alert("Увага! Дія, яку ви збираєтеся виконати, є небезпечною!");
+if (confirm("Ви впевнені, що хочете продовжити?")) {
+  alert("Дякую за підтвердження!");
+} else {
+  alert("Дію відмінено!");
+}
